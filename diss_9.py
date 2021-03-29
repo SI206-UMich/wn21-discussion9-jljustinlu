c8d63648aa99e7b961fb46d525ae73eaf126eab8
jljustinlu <justinlu@umich.edu>
2021-03-29 13:34:32 -0400

    attempted

--- diss_9.py
+++ diss_9.py
@@ -6,34 +6,54 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+    pokemon = soup.find("div", class_ = "infocard-list infocard-list-pkmn-lg")
+    cards = pokemon.find_all("span", class_ = "infocard-lg-img")
+    card = cards[3]
+    link = card.find("a")["href"]
+    return "https://pokemondb.net" + link
+
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
-    url = 'https://pokemondb.net/pokedex/'+pokemon
+    egg_moves = []
+    url = 'https://pokemondb.net/pokedex/' + pokemon
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    moves = soup.find_all("a", class_ = "ent-name")
+    for move in moves:
+        egg_moves.append(move.text)
+    
+    return egg_moves
+
+
     #add code here
 
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
 def findLetters(sentences):
     # initialize an empty list
-    
+    times = []
 
     # define the regular expression
-    
+    exp = r"@\d{1,2}\s?[ap]m"
 
     # loop through each sentence or phrase in sentences
-    
+    for sentence in sentences:
+        
 
     # find all the words that match the regular expression in each sentence
+        matches = re.findall(exp, sentence)
        
 
     # loop through the found words and add the words to your empty list
+        for match in matches:
+            replacement = match.replace("@", "")
+            times.append(replacement)
 
 
     #return the list of the last letter of all words that begin or end with a capital letter
-
+    return times
 
 
 def main():
